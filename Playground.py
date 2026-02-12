9f70ab2d1525df62a4dc2096e06274db51599d7c
WoutervanWijck <woutervanwijck@ziggo.nl>
2026-02-12 15:25:34 +0100

    Sidebranch testen

--- Playground.py
+++ Playground.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-Testing = 'dit is gewoon even testen'
-print(Testing)
+Testing_Sidebranch = 'dit is gewoon even testen'
+print(Testing_Sidebranch)
 
 df = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(df)
