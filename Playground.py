cd1cff4537098198dd16ac7024f3f7ba59560964
WoutervanWijck <woutervanwijck@ziggo.nl>
2026-02-12 14:15:21 +0100

    Dit is de 2e update

--- Playground.py
+++ Playground.py
@@ -1,6 +1,10 @@
 import numpy as np
+import pandas as pd
 Testing = 'dit is gewoon even testen'
 print(Testing)
 
 df = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(df)
+
+df2 = pd.DataFrame(df, columns=['Kolom 1', 'Kolom 2', 'Kolom 3'])
+print(df2)
