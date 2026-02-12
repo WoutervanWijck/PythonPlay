86ce2e8b2793dd766500f876933a57bbab0dfcae
WoutervanWijck <woutervanwijck@ziggo.nl>
2026-02-12 14:35:40 +0100

    dit is de 3e aanpassing

--- Playground.py
+++ Playground.py
@@ -7,4 +7,8 @@ df = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(df)
 
 df2 = pd.DataFrame(df, columns=['Kolom 1', 'Kolom 2', 'Kolom 3'])
+df3 = df2
+df2.insert(0, "Kolom 0", [10, 11, 12])
+
 print(df2)
+print(df3)
